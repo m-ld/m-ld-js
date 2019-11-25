04762f06bfd439cd0fec077dd75bec78c56ead19
George Svarovsky <gsvarovsky@gmail.com>
2019-11-25 18:31:58 +0000

    Standard blocks

--- test/blocks.test.ts
+++ test/blocks.test.ts
@@ -15,3 +15,13 @@ test('Block ordering is unimportant', () => {
   expect(first.next('1').next('2').id
     .equals(first.next('2').next('1').id)).toBe(true);
 });
+
+test('Standard block 1', () => {
+  expect(HashStringBagBlock.genesis('1').id.encode())
+    .toBe('a4ayc/80/OGda4BO/1o/V0etpOqiLx1JwB5S3beHW0s=');
+});
+
+test('Standard block 2', () => {
+  expect(HashStringBagBlock.genesis('1').next('2').id.encode())
+    .toBe('P/oQriWTE9B9qtnAivWcWklJrMNY9Dzamli5TKObBoA=');
+});
