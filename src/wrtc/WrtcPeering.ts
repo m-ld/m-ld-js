28fa9fc94d80151502b05235da47f4142b7646a1
George Svarovsky <gsvarovsky@gmail.com>
2021-04-12 19:45:47 +0100

    Refactoring and prevented extraneous channels due to dangling signals

--- src/wrtc/WrtcPeering.ts
+++ src/wrtc/WrtcPeering.ts
@@ -1,9 +1,10 @@
 import { Logger } from 'loglevel';
 import * as SimplePeer from 'simple-peer';
-import { Future, getIdLogger } from '../engine/util';
+import { getIdLogger } from '../engine/util';
 import type { Instance as Peer, Options as PeerOpts, SignalData } from 'simple-peer';
 import type { MeldConfig } from '..';
 import type { SubPubsub, NotifyParams } from '../engine/PubsubRemotes';
+import { timer } from 'rxjs';
 
 export type CreatePeer = (opts?: PeerOpts) => Peer;
 
@@ -29,7 +30,7 @@ class UnavailableError extends Error {
 export class WrtcPeering {
   private readonly id: string;
   private readonly config: RTCConfiguration;
-  private readonly timeout: (callback: () => void) => ReturnType<typeof setTimeout>;
+  private readonly networkTimeout: number;
   private client: PeerSignaller;
   private readonly peers: {
     [channelId: string]: { connected?: Promise<unknown>, peer?: Peer }
@@ -41,7 +42,7 @@ export class WrtcPeering {
     readonly available = SimplePeer.WEBRTC_SUPPORT) {
     this.id = config['@id'];
     this.config = config.wrtc ?? {};
-    this.timeout = callback => setTimeout(callback, config.networkTimeout ?? 5000);
+    this.networkTimeout = config.networkTimeout ?? 5000;
     this.log = getIdLogger(this.constructor, this.id, config.logLevel ?? 'info');
   }
 
@@ -54,7 +55,7 @@ export class WrtcPeering {
     const outbound = fromId === this.id;
     const peerId = outbound ? toId : fromId;
     if (!this.available)
-      this.setUnavailable(peerId, channelId, true);
+      this.dispose(peerId, channelId, true, 'peering unavailable.');
     const { connected, peer } =
       this.peer(peerId, channelId, outbound);
     if (peer == null)
@@ -74,12 +75,12 @@ export class WrtcPeering {
     // Note that a signal can arrive before the peer has been set up.
     if (!(channelId in this.peers) && (!this.available || data.unavailable)) {
       // Peering not possible. Remember this channel as unavailable.
-      this.setUnavailable(fromId, channelId, !data.unavailable);
+      this.dispose(fromId, channelId, !data.unavailable, 'marked unavailable');
     } else {
       // Get an existing peer or create a new one
       const { peer } = this.peer(fromId, channelId);
       if (peer == null)
-        this.log.warn(`${channelId} is marked unavailable but receiving signals`);
+        this.log.debug(`Disposed channel ${channelId} peer ${fromId} received signal`);
       else if (data.unavailable)
         peer.destroy(new UnavailableError(fromId));
       else
@@ -87,50 +88,51 @@ export class WrtcPeering {
     }
   }
 
-  private setUnavailable(peerId: string, channelId: string, signal: boolean) {
-    // Peering has failed, but the signalling channel may still be open
-    if (signal)
+  private dispose(peerId: string, channelId: string, signalUnavailable: boolean, ...msg: any[]) {
+    this.log.debug(`Channel ${channelId} peer ${peerId} disposed:`, ...msg);
+    // If peering has failed, the signalling channel may still be open
+    if (signalUnavailable)
       this.client.signal(peerId, channelId, { unavailable: true })
         .catch(err => this.log.warn(err));
     this.peers[channelId] = {};
     this.timeout(() => this.forget(channelId));
   }
 
-  private forget(channelId: string) {
-    delete this.peers[channelId];
-  }
-
   private peer(peerId: string, channelId: string, initiator = false) {
     if (channelId in this.peers) {
       return this.peers[channelId];
     } else {
-      const logName = `Peer ${peerId} on channel ${channelId}`;
       const timer = this.timeout(() =>
         peer.destroy(new Error('connection timeout exceeded.')));
       const peer = this.createPeer({ config: this.config, initiator });
+      this.log.debug(`Channel ${channelId} peer ${peerId}`, 'created.');
       const connected = new Promise((resolve, reject) => {
         peer.on('connect', resolve);
         // If an error is emitted, destruction is underway
         peer.on('error', reject);
       }).then(() => {
-        this.log.debug(logName, 'connected.');
+        this.log.debug(`Channel ${channelId} peer ${peerId}`, 'connected.');
       }).catch(err => {
-        this.log.debug(logName, 'errored with', err);
         // Don't signal the error to the peer if they are unavailable
-        this.setUnavailable(peerId, channelId, !(err instanceof UnavailableError));
+        this.dispose(peerId, channelId, !(err instanceof UnavailableError), 'errored', err);
         throw err;
-      }).finally(() => clearTimeout(timer));
+      }).finally(() => timer.unsubscribe());
       peer.on('signal', (data: SignalData) =>
         this.client.signal(peerId, channelId, data)
           .catch(err => peer.destroy(err)));
       peer.on('data', (data: Buffer) =>
         this.client.notify(channelId, data));
-      peer.on('close', () => {
-        this.log.debug(logName, 'closed.');
-        // If an error occurred, the channel will be unavailable
-        connected.then(() => this.forget(channelId), () => { });
-      });
+      peer.on('close', () =>
+        this.dispose(peerId, channelId, false, 'closed.'));
       return this.peers[channelId] = { connected, peer };
     }
+  }
+
+  private forget(channelId: string) {
+    delete this.peers[channelId];
+  }
+
+  private timeout(cb: () => void) {
+    return timer(this.networkTimeout).subscribe(cb);
   }
 }
